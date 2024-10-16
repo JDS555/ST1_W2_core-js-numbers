7ebc7a40f60701cba62550df662ee0a102b3edaa
JDS555 <nfive.5.101@gmail.com>
2024-10-16 15:57:09 +0300

    feat: praxis 2024-10-16

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -194,6 +194,8 @@ function parseNumberFromString(value) {
   return numberFromString;
 }
 
+// N9+++
+
 /**
  * Returns a diagonal length of the rectangular parallelepiped given by its sides a,b,c.
  *
@@ -207,9 +209,12 @@ function parseNumberFromString(value) {
  *   3,3,3   => 5.196152422706632
  *   1,2,3   => 3.741657386773941
  */
-function getParallelepipedDiagonal(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getParallelepipedDiagonal(a, b, c) {
+  const parallelepipedDiagonal = Math.sqrt(a ** 2 + b ** 2 + c ** 2);
+  return parallelepipedDiagonal;
 }
+
+// N10
 
 /**
  * Returns the number rounded to specified power of 10.
